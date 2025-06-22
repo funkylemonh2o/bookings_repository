b962ade2773497f736bad0f93f2323236103374a
funkylemonh2o <150464518+funkylemonh2o@users.noreply.github.com>
2025-06-22 16:32:33 +0200

    finished project? commit

--- bookings_project/booking_app/admin.py
+++ bookings_project/booking_app/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
-from .models import Booking, Location, Feedback
+from .models import Location, Booking, Feedback
 
-# Register your models here.
-admin.site.register(Location)
-admin.site.register(Booking)
-admin.site.register(Feedback)
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'location', 'start_time', 'end_time', 'creation_time')
+    list_filter = ('location', 'start_time')
+    search_fields = ('user__username', 'location__number')
+    ordering = ('-start_time',)
+
+    # These fields will be shown in the form when editing
+    fields = ('user', 'location', 'start_time', 'end_time')
+
+    # Use dropdowns for foreign keys
+    raw_id_fields = ('user', 'location')
+
+    # Allow deletion directly from list view or detail view
+    actions = ['delete_selected']
+
+    # Optional: prevent changing user/location after creation
+    # def get_readonly_fields(self, request, obj=None):
+    #     if obj:  # editing an existing object
+    #         return ('user', 'location')
+    #     return ()
